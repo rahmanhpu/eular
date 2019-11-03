d584b3527f20b5127a5a9a20a9b2af3a62f16264
Homaidur Rahman <rahmanhpu@gmail.com>
2019-11-04 01:03:17 +0530

    this is problem 3 of project Eular

---
+++ problem_03.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Problem 3:
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+"""
+
+def get_dividers(n):
+	i = 1
+	dividers = []
+	while i < n:
+		if n%i == 0:
+			dividers.append(i)
+			n = n/i
+			i=i+1
+		else:
+			i=i+1
+	dividers.append(n)
+	return dividers
+
+print(get_dividers(13195))
+
+print(get_dividers(600851475143)[-1])
